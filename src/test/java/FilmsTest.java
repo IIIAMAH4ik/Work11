337647c20f41e217c2a422af89dbd5fe06f48463
IIIAMAH4ik <tembatokmakov43@gmail.com>
2023-04-17 13:53:48 +0300

    final commit

--- src/test/java/FilmsTest.java
+++ src/test/java/FilmsTest.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.Test;
 
 public class FilmsTest {
     @Test
-    public void test() {
+    public void testAll() {
         FilmsManager manager = new FilmsManager();
 
         manager.addFilm("Film I");
@@ -11,5 +11,17 @@ public class FilmsTest {
 
         String[] expected = {"Film I", "Film II", "Film III"};
         String[] actual = manager.findAll();
+    }
+
+    @Test
+    public void testLast() {
+        FilmsManager manager = new FilmsManager();
+
+        manager.addFilm("Film I");
+        manager.addFilm("Film II");
+        manager.addFilm("Film III");
+
+        String[] expected = {"Film I", "Film II", "Film III"};
+        String[] actual = manager.findLast();
     }
 }
